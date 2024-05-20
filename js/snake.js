d89a2ee71c10c5b26a62cb6937f78c50009725a4
Christopher Yuen <chriskaijoyu@gmail.com>
2024-05-20 12:20:06 -0400

    update

--- js/snake.js
+++ js/snake.js
@@ -4,4 +4,16 @@ const canvas = document.getElementById('gameCanvas');
 // 2d context
 const context = convas.getContext('2d');
 
-// draw 300x300px black border canvas
+// snake
+let snake = [{x: 150, y:150}, {x: 140, y:150}, {x: 130, y:150}, {x: 120, y:150}, {x: 110, y:150}];
+
+function drawSnakePart(snakePart) {
+    context.fillStyle = 'lightgreen';
+    context.strokeStyle = 'darkgreen';
+    context.fillRect(snakePart.x, snakePart.y, 10, 10);
+    context.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() {
+    snake.forEach(drawSnakePart);
+}
