d286fe1a2647422d41e25e117a3d136be2a6b9e6
Christopher Yuen <chriskaijoyu@gmail.com>
2024-05-20 13:59:07 -0400

    update

--- js/snake.js
+++ js/snake.js
@@ -2,7 +2,7 @@
 const canvas = document.getElementById('gameCanvas');
 
 // 2d context
-const context = convas.getContext('2d');
+const context = canvas.getContext('2d');
 
 // snake
 let snake = [{x: 150, y:150}, {x: 140, y:150}, {x: 130, y:150}, {x: 120, y:150}, {x: 110, y:150}];
